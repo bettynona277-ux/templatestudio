b1da962af221464215a44a231dfe05df0ae78102
bettynona277-ux <bettynona277@gmail.com>
2026-06-09 12:19:11 -0500

    Add files via upload

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'disenos-streaming-v137';
+const CACHE_NAME = 'disenos-streaming-v138';
 const CLOUDINARY_CACHE = 'disenos-streaming-cloudinary-v1';
 const ASSETS = [
   '/index-mobile.html',
