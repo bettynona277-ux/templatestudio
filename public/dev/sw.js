734deac137616afb3285044e533071fdfdb3d38a
bettynona277-ux <bettynona277@gmail.com>
2026-07-20 19:08:45 -0500

    Add files via upload

--- public/dev/sw.js
+++ public/dev/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'disenos-streaming-v193';
+const CACHE_NAME = 'disenos-streaming-v194';
 const CLOUDINARY_CACHE = 'disenos-streaming-cloudinary-v1';
 const ASSETS = [
   '/manifest.json',
